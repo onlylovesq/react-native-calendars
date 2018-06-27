f04dea531b8cfde86eea60e4f96200e0f18434d3
孙全 <943788231@qq.com>
2018-06-27 09:29:11 +0800

    改样式

--- src/calendar/header/style.js
+++ src/calendar/header/style.js
@@ -1,17 +1,18 @@
-import {StyleSheet, Platform} from 'react-native';
-import * as defaultStyle from '../../style';
+import { StyleSheet, Platform } from "react-native";
+import * as defaultStyle from "../../style";
 
-const STYLESHEET_ID = 'stylesheet.calendar.header';
+const STYLESHEET_ID = "stylesheet.calendar.header";
 
-export default function(theme={}) {
-  const appStyle = {...defaultStyle, ...theme};
+export default function(theme = {}) {
+  const appStyle = { ...defaultStyle, ...theme };
   return StyleSheet.create({
     header: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
+      flexDirection: "row",
+      justifyContent: "space-between",
       paddingLeft: 10,
       paddingRight: 10,
-      alignItems: 'center'
+      alignItems: "center",
+      marginBottom: 18
     },
     monthText: {
       fontSize: appStyle.textMonthFontSize,
@@ -35,14 +36,14 @@ export default function(theme={}) {
     },
     week: {
       marginTop: 7,
-      flexDirection: 'row',
-      justifyContent: 'space-around'
+      flexDirection: "row",
+      justifyContent: "space-around"
     },
     dayHeader: {
       marginTop: 2,
       marginBottom: 7,
       width: 32,
-      textAlign: 'center',
+      textAlign: "center",
       fontSize: appStyle.textDayHeaderFontSize,
       fontFamily: appStyle.textDayHeaderFontFamily,
       color: appStyle.textSectionTitleColor
